5e12ba4f110fd69124b1d30530df277323a43411
LeviMatheus <levi.matheus@outlook.com>
2019-11-05 19:22:20 -0300

    Lendo base de dados

--- bayesian.py
+++ bayesian.py
@@ -1,3 +1,23 @@
+#!/usr/bin/env python3
+import numpy as np
 import sys
-param = sys.argv[1:]
-print(param)
+
+class bayes:
+
+    def __init__(self):
+        # dataset
+        self.ds = None
+        self.d = None
+        self.carregaDs()
+
+    def carregaDs(self):
+        dtst = sys.argv[1:]
+        print(dtst)
+        arquivo = open(dtst[0], "r")
+        linhas = arquivo.read().split("\n")
+        linhas.pop()
+        for linha in linhas:
+            print(linha)	
+        arquivo.close()
+
+bayes()
