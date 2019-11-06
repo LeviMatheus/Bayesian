1287f0dca996b68d9619b1425e2c65660bb3417b
LeviMatheus <levi.matheus@outlook.com>
2019-11-05 21:14:22 -0300

    Já está criando vetor de atributos de instancias

--- bayesian.py
+++ bayesian.py
@@ -15,19 +15,36 @@ class bayes:
         print("\n Analisando base de dados: ", entrada, "\n") # MOSTRA A ENTRADA
         arquivo = open(entrada[0], "r") # ABRE O ARQUIVO DE ENTRADA
         separador = arquivo.read().split("---")
-        #arquivo.close()
+        arquivo.close()
         #for conjunto in dataset:
             #print(conjunto) 
         dataset = separador[0]
         questoes = separador[1]
         print("Dataset: \n", dataset)
-        print("")
+        print("",end="")
         print("Questões: ", questoes)
 
-        instancias = dataset.split("\n") # LE CADA UMA DAS LINHAS E SEPARA COM /N
-        instancias.pop() 
-        for i in instancias:
-            print(i) # MOSTRA AS LINHAS	
-        arquivo.close() # FECHA O ARQUIVO#
+        #CRIANDO VETOR DE INSTANCIAS DO DATASET E DAS QUESTOES
+
+        instancias_dataset = dataset.split("\n") # LE CADA UMA DAS LINHAS E SEPARA COM /N
+        instancias_dataset.pop() 
+        for i in range(len(instancias_dataset)):
+            if i != 0: #PARA N PRINTAR O TITULO DOS ATRIBUTOS
+                print(instancias_dataset[i]) # MOSTRA AS LINHAS	DAS INSTANCIAS DO DATASET
+
+        instancias_questoes = questoes.split("\n") # LE CADA UMA DAS LINHAS E SEPARA COM /N
+        instancias_questoes.pop() 
+        for i in instancias_questoes:
+            print(i) # MOSTRA AS LINHAS	DAS INSTANCIAS DO DATASET
+
+        print("\n Atributos da instância: \n")
+        #CRIAR VETOR DE ATRIBUTOS DE INSTANCIAS (dados)
+        dados = []
+        for i in instancias_dataset:
+            dados.append(i.split(" "))
+        print(dados)
+
+        #print(dados[0][0] + dados[1][0])
+        #print(dados[0][1] + dados[1][1])
 
 bayes()
