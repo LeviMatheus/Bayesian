4551f1e5e52f55088ac11bee244f7d8f62eda20c
LeviMatheus <levi.matheus@outlook.com>
2019-11-15 20:36:20 -0300

    atualização 15/11

--- bayesian.py
+++ bayesian.py
@@ -20,20 +20,24 @@ class bayes:
             #print(conjunto) 
         dataset = separador[0]
         questoes = separador[1]
-        print("Dataset: \n", dataset)
-        print("",end="")
-        print("Questões: ", questoes)
+        #print("Dataset: \n", dataset)
+        #print("",end="")
+        #print("Questões: ", questoes)
 
         #CRIANDO VETOR DE INSTANCIAS DO DATASET E DAS QUESTOES
 
         instancias_dataset = dataset.split("\n") # LE CADA UMA DAS LINHAS E SEPARA COM /N
-        instancias_dataset.pop() 
+        instancias_dataset.pop()
+        print("Dataset: \n")
         for i in range(len(instancias_dataset)):
             if i != 0: #PARA N PRINTAR O TITULO DOS ATRIBUTOS
                 print(instancias_dataset[i]) # MOSTRA AS LINHAS	DAS INSTANCIAS DO DATASET
+        
+        print("\n")
 
         instancias_questoes = questoes.split("\n") # LE CADA UMA DAS LINHAS E SEPARA COM /N
-        instancias_questoes.pop() 
+        instancias_questoes.pop()
+        print("Questões:") 
         for i in instancias_questoes:
             print(i) # MOSTRA AS LINHAS	DAS INSTANCIAS DO DATASET
 
@@ -44,7 +48,7 @@ class bayes:
             dados.append(i.split(" "))
         print(dados)
 
-        #print(dados[0][0] + dados[1][0])
-        #print(dados[0][1] + dados[1][1])
+        print(dados[0][0] + dados[1][0])
+        print(dados[0][1] + dados[1][1])
 
 bayes()
