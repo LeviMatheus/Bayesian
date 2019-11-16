0aa752d0c63b3293fcc492e55cc41def38ec3d6e
LeviMatheus <levi.matheus@outlook.com>
2019-11-16 00:26:33 -0300

    calculo inicial das probabilidades

--- bayesian.py
+++ bayesian.py
@@ -11,6 +11,10 @@ class bayes:
         self.carregaDs()
 
     def carregaDs(self):
+
+        def column(self, matrix, i):
+            return [row[i] for row in matrix]
+
         entrada = sys.argv[1:] # PEGA O SEGUNDO INDICE COMO ENTRADA
         print("\n Analisando base de dados: ", entrada, "\n") # MOSTRA A ENTRADA
         arquivo = open(entrada[0], "r") # ABRE O ARQUIVO DE ENTRADA
@@ -57,9 +61,51 @@ class bayes:
         totalLinhas = len(dados) #CONTANDO QTD DE INSTANCIAS
         #print(totalLinhas)
 
-        classes = []
+        coluna = []
         for i in dados:
-            classes.append(i[-1])
+            coluna.append(i[-1])
         #print(classes)
+        classes = set(coluna)
+        classes = list(classes)
+        qtd0 = coluna.count(classes[0])#countagem de instancias
+        qtd1 = coluna.count(classes[1])
+
+        print("\n")
+        print("Classe 0: ", classes[0], "Frequência: ", qtd0)
+        print("Classe 1: ", classes[1], "Frequência: ", qtd1)
+
+        p0 = qtd0/totalLinhas #porcentagem classe 0
+        p1 = qtd1/totalLinhas #porcentagem classe 1
+        print("Porcentagem da classe 0: ", p0)
+        print("Porcentagem da classe 1: ", p1)
+
+        qtdatributos = len(rotulos)-1
+        print("Quantidade de atributos: ", qtdatributos)
+
+        valAtributos = []
+        for i in range(qtdatributos):
+            valAtributos.append(set(column(self, dados,i)))
+        print(set(column(self, dados,0)))
+        #print(atributos2)
+
+        pa1 = 0
+        probAtribC0 = [] # vetor com a probabilidade de cada atributo para classe 0
+        probAtribC1 = [] # vetor com a probabilidade de cada atributo para classe 1
+        #verificar se dados[i][-1] = determinada classe
+        for instancia in dados:
+            #print(instancia)
+            if instancia[-1] == classes[0]:#se for da classe 0
+                print(instancia[-1])
+            #    print(instancia[0])
+            #    if(questoes[0][0] == column(self,instancia,0)[0]):#valAtributos[0][0], column(self,dados,0
+                    #print("verificando", questoes[0][0], " igual", column(self,instancia,0)[0])
+            #        pa1 += 1
+
+        #print(instancia[0][-1])
+        #print(questoes[0][0])
+        #print(classes[0])
+        #print(column(self,dados,0)[3])
+        #print(dados)
+        #print(pa1)
 
 bayes()
